9212b376931f55d449f17b13e7a9feab64295b78
GlukoLyoFF <glukolyoff@gmail.com>
2021-02-24 12:08:47 +0000

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode=true) {
+    this.reverse = (!mode) ? true : false;
+  }
+  encrypt(str=-1, key=-1, mode=true) {
+    if (str == -1 || key == -1) {
+      throw new Error('No args');
+    };
+    let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        res = '';
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let key_stack = [];
+    for (let i = 0; i < str.length; i++) {
+      if (key_stack.length == 0) {key_stack = key.split('').reverse()};
+      let stri = abc.indexOf(str[(i >= str.length) ? i % str.length : i]);
+      if (stri == -1) {
+        res += str[i];
+        continue;
+      }; 
+      let keyi = abc.indexOf(key_stack.pop());
+      keyi = (mode) ? keyi : -keyi; // encrypt : decrypt
+      res += abc[((abc.length + stri + keyi) % abc.length)];
+    };
+    return ((this.reverse == true) ? res.split('').reverse().join('') : res);
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str=-1, key=-1) {
+    if (str == -1 || key == -1) {
+      throw new Error('No args');
+    };
+    return this.encrypt(str, key, false);
   }
 }
 
